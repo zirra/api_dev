41d343290a8b72c6213345d73c2129a33acd747a
zirra <msullivan@heysulli.com>
2022-06-18 08:41:01 -0500

    removed commented out

--- controllers/content/aws.controller.js
+++ controllers/content/aws.controller.js
@@ -96,19 +96,6 @@ AwsManager = {
     } else {
       res.status(404).send('Not Found')
     }
-    
-    /*
-    var params = {
-      Key: fileName,
-      Body: fileData,
-    }
-    s3bucket.upload(params, function (err, res) {               
-      if(err)
-        console.log("Error in uploading file on s3 due to "+ err)
-      else    
-        console.log("File successfully uploaded.")
-    })
-    */
   }
 }
 
